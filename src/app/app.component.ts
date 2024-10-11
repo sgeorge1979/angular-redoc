05c8e156dccbf974506a55bf923f118c8909a857
Sudhakar George <149185658+sgeorge1979@users.noreply.github.com>
2024-10-11 11:11:32 -0400

    Initial Checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import * as data from '../assets/redoc-sample.json';
+declare var Redoc: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'angular-redoc';
+  data: any = data;
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit() {
+    this.initDocs();
+  }
+
+  initDocs(){
+    const container = this.el.nativeElement.querySelector('#redoc');
+    Redoc.init(this.data, {
+    scrollYOffset: 60,
+    hideDownloadButton: true
+   }, container);
+  }
 }
